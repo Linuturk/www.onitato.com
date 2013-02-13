142ff59547fe60d38f15c3542a8d5d3594384807
Justin Phelps <linuturk@onitato.com>
2013-02-12 20:15:36 -0600

    Modifed the formatting to solve the following pep8 errors: cf_pyrax.py:2: W402 'time' imported but unused cf_pyrax.py:2:13: E401 multiple imports on one line cf_pyrax.py:21:80: E501 line too long (81 > 79 characters) cf_pyrax.py:22:80: E501 line too long (108 > 79 characters)

--- cf_pyrax.py
+++ cf_pyrax.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import pyrax, time
+import pyrax
 
 # Set variables for the various options
 username = ""
@@ -18,6 +18,8 @@ pyrax.set_credentials(username, apikey)
 cf_ord = pyrax.cloudfiles
 
 # Upload the entire folder to the cloud files container.
-print "Syncing all objects to %s container from folder %s." % (container, folder)
-cf_ord.sync_folder_to_container(folder, container, delete=True, include_hidden=True, ignore_timestamps=True)
+print("Syncing all objects to %s container from folder %s." %
+      (container, folder))
+cf_ord.sync_folder_to_container(folder, container, delete=True,
+                                include_hidden=True, ignore_timestamps=True)
 print "Sync complete."

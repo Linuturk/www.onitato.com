3d390343e173e8a0b9c82d2bfd742b3cceb45f16
Justin Phelps <juphelps@netflix.com>
2017-08-15 20:18:08 -0700

    check for the file

--- install-hugo.sh
+++ install-hugo.sh
@@ -7,7 +7,7 @@ HUGO_VERSION=0.26
 HUGO_DOWNLOAD=hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 
 # Install Hugo if not already cached or upgrade an old version.
-if [ ! -e $HOME/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
+if [ ! -f $HOME/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
 	if [ ! -f /tmp/${HUGO_DOWNLOAD} ]; then
 		wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD} -O /tmp/${HUGO_DOWNLOAD}
 	fi

faf3dff0ca28e3e2ba49d9fa95e778532dc79cf4
Justin Phelps <juphelps@netflix.com>
2018-10-08 13:00:57 -0700

    change hugo bin location

--- install-hugo.sh
+++ install-hugo.sh
@@ -3,14 +3,15 @@
 set -x
 set -e
 
-HUGO_VERSION=0.31.1
+HUGO_BIN=/bin/hugo
+HUGO_VERSION=0.49
 HUGO_DOWNLOAD=hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 
 # Install Hugo if not already cached or upgrade an old version.
-if [ ! -f $HOME/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
+if [ ! -f $HUGO_BIN ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
 	if [ ! -f /tmp/${HUGO_DOWNLOAD} ]; then
 		wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD} -O /tmp/${HUGO_DOWNLOAD}
 	fi
   	tar xvzf /tmp/${HUGO_DOWNLOAD} hugo
-  	mv hugo $HOME/bin/hugo
+  	mv hugo $HUGO_BIN
 fi
